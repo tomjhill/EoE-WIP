12ba12b229dc2b2aa4c910ca95f6a72f1dbdf223
TomJHill <tomjhill@me.com>
2018-11-26 14:52:38 +0000

    Update pages/examples.js
    fixed

--- pages/examples.js
+++ pages/examples.js
@@ -3,39 +3,13 @@
 PC.pages.examples = {}
 
 /**
-<<<<<<< HEAD
- * Renders the elements list page
+ * Renders the examples list page
  *
- * The elements list page can optionally be filtered by a category, and will
- * then only show elements from that category. This is only used from the
- * categories page, in order to render lists of elements with only elements
+ * The examples list page can optionally be filtered by a category, and will
+ * then only show examples from that category. This is only used from the
+ * categories page, in order to render lists of examples with only examples
  * from a selected category.
  */
-
-PC.pages.examples.renderHTML = function (params) {
-
-
-  var query = {
-    content_type: PC.config.exampleContentTypeId
-  }
-
-  if (params && params.elementId) {
-
-    query['fields.element.sys.id[in]'] = params.elementId
-
-  }
-
-  return PC.contentfulClient.getEntries(query)
-
-  .then(function (entries) {
-    return renderExamples(entries.items)
-
-
-  })
-
-
-
-
 
 PC.pages.examples.renderHTML = function (category, element) {
 
@@ -70,18 +44,13 @@ console.log("examples "+ category)
 
 
   })
->>>>>>> a9af1a4936e2306ff201bbf95d90e243101820d6
 
 
 
 }
 
 function renderCategoryTitle(examples) {
-<<<<<<< HEAD
-  return '<h1 class="category-title">Examples</h1>' +
-=======
   return '<h1 class="example-title">examples</h1>' +
->>>>>>> a9af1a4936e2306ff201bbf95d90e243101820d6
     '<div class="examples">' +
     examples.map(renderSingleExample).join('\n') +
     '</div>'
@@ -96,41 +65,14 @@ function renderExamples(examples) {
 
 function renderSingleExample(example) {
   var fields = example.fields
-<<<<<<< HEAD
+  var exampleId = example.sys
   return '<div class="example-in-list">' +
     '<div class="example-details">' +
-      renderExampleDetails(fields) +
-=======
-  var exampleId = example.sys
-  return '<div class="example-in-list" >' +
-    '<div class="example-details">' +
       renderExampleDetails(fields, exampleId) +
->>>>>>> a9af1a4936e2306ff201bbf95d90e243101820d6
     '</div>' +
   '</div>'
 }
 
-<<<<<<< HEAD
-function renderExampleDetails(fields) {
-  return renderExampleHeader(fields)
-
-}
-
-function renderExampleHeader(fields) {
-  return '<div class="example-header">' +
-    '<h2>' +
-      '<a href="example/' + fields.description + '" data-nav>' +
-        fields.name +
-      '</a>'+
-    '</h2>' +
-  '</div>'
-}
-
-function renderImage(image, caption) {
-  if(image && image.fields.file) {
-    return '<a href="element/' + caption + '" data-nav>' +
-      '<img src="' + image.fields.file.url + '" width="150" height="150" />' +
-=======
 function renderExampleDetails(fields, exampleId) {
   return renderExampleHeader(fields, exampleId)+
     '<div class="example-caption">'+
@@ -188,7 +130,6 @@ function renderImage(image, caption) {
   if(image && image.fields.file) {
     return '<a href="example/' + caption + '" data-nav>' +
       '<img class="example-image" src="' + image.fields.file.url + '"  />' +
->>>>>>> a9af1a4936e2306ff201bbf95d90e243101820d6
     '</a>'
   } else {
     return ''
